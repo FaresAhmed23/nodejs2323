d087454eaa2bb5ba6297d0d742be979eadc8b1b2
Fares Ahmed <fero32517@gmail.com>
2025-08-20 01:24:55 +0300

    sd

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Body parsing middleware
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Logging middleware
